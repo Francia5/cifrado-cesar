5a6eab9b7aca1b97057e1094b10b10df67585467
francia5 <francia.va.or@gmail.com>
2018-04-11 11:09:00 -0500

    se suben los archivos finalizados y actualizados junto con los comentarios requeridos en las lineas de codigo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 do{
-  var pickOne = prompt("Elige 1 si quieres cifrar una frase, y elige 2 si quieres descifrar una frase");// Por medio de este prompt podemos saber si el usuario quiere cifrar o descifrar alguna frase
+  var pickOne = prompt("Elige 1 si quieres cifrar una frase \nElige 2 si quieres descifrar una frase");// Por medio de este prompt podemos saber si el usuario quiere cifrar o descifrar alguna frase
   var phrase = prompt("Por favor Ingresa una frase sin espacios ni numeros");//obtenemos la frase cifrada o descifrada y la guardamos en la varible
 
 console.log(phrase);
@@ -9,7 +9,7 @@ console.log(phrase);
   } else {
       decipher(phrase, 33);
   }
-}while ((phrase) === " " || isNaN (phrase) === false);
+}while ((phrase) === " " || isNaN (phrase) === false); // Si encuentra un espacio vacio o un numero en la frase seguira preguntando hasta ingresar la frase de la manera solicitada.
 
 
 
@@ -20,33 +20,35 @@ function cipher(phrase){ // Con esta funcion cifraremos la frase ingresada por e
       var letterMay = (codeNumber - 65 + 33) % 26 + 65;
       var letterMin = (codeNumber - 97 + 33) % 26 + 97;
 
-        if (codeNumber >= 65 && codeNumber <= 90) {
+        if (codeNumber >= 65 && codeNumber <= 90) { //Condicionamos y seccionamos, si codeNumber es mayor o igual a la posicion 65 o menor o igual que 90 en el codigo ASCII seran letras Mayusculas.
           str += String.fromCharCode(letterMay);
 
         } else {
-          (codeNumber >= 97 && codeNumber <= 122)
+          (codeNumber >= 97 && codeNumber <= 122) //Condicionamos y seccionamos, si codeNumber es mayor o igual a la posicion 97 o menor o igual que 122 en el codigo ASCII seran letras Minisculas.
           str += String.fromCharCode(letterMin);
         }
     }
 
+    console.log(str); // str Nos muestra la frase cifrada
     document.write("Tu frase cifrada es " + str); //Imprimimos en la pantalla la frase cifrada para el usuario que guardamos en la variable str.
 }
 
-  function decipher(cipher){
-    var str= "";
-      for (var i = 0; i < phrase.length; i++) {
+  function decipher(cipher){ // Por medio de esta funcion descifraremos la frase que el usuario ingrese
+    var str= ""; //Aqui se guardara la frase descifrada
+      for (var i = 0; i < phrase.length; i++) { //Iterar el string para poder obtener el numero de la letra dentro del codigo ascii para posteriormente poder recorrernos 33 espacios.
         var codeNumber = phrase.charCodeAt(i);
-        var letterMay = (((codeNumber + 65 - 33) % 26)+ 65);
+        var letterMay = (((codeNumber + 65 - 33) % 26)+ 65); //En el caso de la funcion descifrar requerimos recorrernos hacia la izquierda 33 espacios ya que en el anterior los avanzamos.
         var letterMin = (((codeNumber + 85 - 33) % 26)+ 97);
 
-          if (codeNumber >= 65 && codeNumber <= 90) {
+          if (codeNumber >= 65 && codeNumber <= 90) { //Condicionamos y seccionamos, si codeNumber es mayor o igual a la posicion 65 o menor o igual que 90 en el codigo ASCII seran letras Mayusculas.
             str += String.fromCharCode(letterMay);
 
           } else {
-            (codeNumber >= 97 && codeNumber <= 122)
+            (codeNumber >= 97 && codeNumber <= 122) //Condicionamos y seccionamos, si codeNumber es mayor o igual a la posicion 97 o menor o igual que 122 en el codigo ASCII seran letras Minisculas.
             str += String.fromCharCode(letterMin);
           }
       }
 
-      document.write("Tu frase descifrada es " + str);
+      console.log(decipher); // str Nos muestra la frase descifrada
+      document.write("Tu frase descifrada es: " + str); //Imprimimos en la pantalla la frase descifrada para el usuario que guardamos en la variable str.
   }
